2f225e4a0f588e5b64237919c85f61967f5583e5
Nazarii Kuntsevych <nazarii.kuntsevych@gmail.com>
2023-01-27 01:15:30 +0200

    CV-4-add-CommentSerializer

--- app/discussions/api/serializers.py
+++ app/discussions/api/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 
-from ..models import Discussion
+from ..models import Discussion, Comment
 
 
 class DiscussionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Discussion
         fields = ['id', 'title', 'content', 'created', 'updated', 'user', 'movie']
+        read_only_fields = ['id']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'content', 'user', 'discussion']
         read_only_fields = ['id']
