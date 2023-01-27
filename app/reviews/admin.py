db7627536d8c5e0390862459b630942a6261a95c
Motorenger <90602764+Motorenger@users.noreply.github.com>
2023-01-27 10:56:12 +0200

    CV-11-reviewslike-admin

--- app/reviews/admin.py
+++ app/reviews/admin.py
@@ -3,5 +3,15 @@ from django.contrib import admin
 from reviews.models import Review, ReviewLikes
 
 
-admin.site.register(Review)
+class ReviewLikesInline(admin.TabularInline):
+    model = ReviewLikes
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    inlines = [
+        ReviewLikesInline,
+    ]
+
+
+admin.site.register(Review, ReviewAdmin)
 admin.site.register(ReviewLikes)
