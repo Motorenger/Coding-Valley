9d4b3155858a8691e67edd5e01a15b50e26987b7
Motorenger <90602764+Motorenger@users.noreply.github.com>
2023-01-27 10:48:20 +0200

    CV-11-review-model

--- app/reviews/admin.py
+++ app/reviews/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from reviews.models import Review
+from reviews.models import Review, ReviewLikes
 
 
 admin.site.register(Review)
+admin.site.register(ReviewLikes)
