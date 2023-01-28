6d59e348430f8ae6aadcf8e14fa1fe803dc85bf0
Motorenger <90602764+Motorenger@users.noreply.github.com>
2023-01-28 19:01:25 +0200

    CV-16-crete-series-season-episode

--- app/whatchlists/models.py
+++ app/whatchlists/models.py
@@ -41,12 +41,11 @@ class Movie(models.Model):
 class Series(models.Model):
     id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=50)
+    year = models.CharField(max_length=9, help_text="Use such format: 2018-2022")
     released = models.DateField(_("Release date"))
-    runtime = models.PositiveIntegerField()
-    genres = models.ManyToManyField(Genre, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="series")
     plot = models.TextField()
-    totalSeasons = models.PositiveInteger()
-
+    total_seasons = models.PositiveIntegerField()
 
     class Meta:
         verbose_name = _("Series")
@@ -54,3 +53,34 @@ class Series(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Episode(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=50)
+    released = models.DateField()
+    episode_num = models.PositiveIntegerField()
+    runtime = models.PositiveIntegerField()
+    plot = models.TextField()
+
+    class Meta:
+        verbose_name = _("Episode")
+        verbose_name_plural = _("Episodes")
+
+    def __str__(self):
+        return self.title
+
+
+class Season(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+    season = models.PositiveIntegerField()
+    total_episodes = models.PositiveIntegerField()
+    series = models.ForeignKey(Series, on_delete=models.CASCADE, related_name="seasons")
+    episodes = models.ManyToManyField(Episode, related_name="season")
+
+    class Meta:
+        verbose_name = _("Season")
+        verbose_name_plural = _("Seasons")
+
+    def __str__(self):
+        return self.name
