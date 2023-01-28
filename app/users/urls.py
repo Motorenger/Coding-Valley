457615dcbb0214c504d2c4f1e97bdf7256f4ffb7
Igor Karuna <ihor.karuna@gmail.com>
2023-01-28 23:25:34 +0200

    CV-9-pep-fix

--- app/users/urls.py
+++ app/users/urls.py
@@ -3,12 +3,11 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
 from users import views
-from users.views import UserTokenObtainPairView
 
 
 urlpatterns = [
     path('', views.getRoutes),
     path('register/', views.RegisterView.as_view(), name='register'),
-    path('token/', UserTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', views.UserTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
