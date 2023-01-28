7e716aebd9ae0a257fd414d39e872e39b41e354b
Igor Karuna <88438873+igorkaruna@users.noreply.github.com>
2023-01-28 14:37:31 +0200

    CV-9-add-default-auth-class

--- app/base/settings.py
+++ app/base/settings.py
@@ -106,6 +106,12 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
