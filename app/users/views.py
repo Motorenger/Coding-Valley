4b30b09bfaeb0e5ff0be6381b906a6fd11ace608
Igor Karuna <ihor.karuna@gmail.com>
2023-01-28 20:35:20 +0200

    CV-9-add-register-view

--- app/users/views.py
+++ app/users/views.py
@@ -1,6 +1,17 @@
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+from users.models import User
+from users.serializers import (
+    UserTokenObtainPairSerializer,
+    UserSerializerWithToken
+)
 
 
 class UserTokenObtainPairView(TokenObtainPairView):
@@ -10,8 +21,30 @@ class UserTokenObtainPairView(TokenObtainPairView):
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
+        'register/'
         'token/',
         'token/refresh/',
     ]
     return Response(routes)
-    
+
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        data = request.data
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        email = data.get('email')
+        password = data.get('password')
+        try:
+            user = User.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                password=make_password(password)
+            )
+            serializer = UserSerializerWithToken(user, many=False)
+        except Exception as e:
+            return Response({'detail':f'{e}'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data)
