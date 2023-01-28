083580f179193414d7822dfb296861eaa508bef4
Igor Karuna <ihor.karuna@gmail.com>
2023-01-28 14:27:04 +0200

    CV-9-initialize-user-urls

---
+++ app/users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from users import views
+from users.views import UserTokenObtainPairView
+
+
+urlpatterns = [
+    path('', views.getRoutes),
+    path('token/', UserTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
