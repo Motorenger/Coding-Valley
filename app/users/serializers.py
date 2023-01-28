70e91b1da1db74b7b179c57ee5932a7e6d56c395
Igor Karuna <ihor.karuna@gmail.com>
2023-01-28 20:34:16 +0200

    CV-9-add-user-token-serializer

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from users.models import User
@@ -23,3 +24,34 @@ class UserTokenObtainPairSerializer(TokenObtainPairSerializer):
         token['last_name'] = user.last_name
         token['email'] = user.email
         return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        serializer = UserSerializerWithToken(self.user).data
+        for key, value in serializer.items():
+            data[key] = value
+
+        return data
+
+
+class UserSerializerWithToken(UserSerializer):
+    access = serializers.SerializerMethodField(read_only=True)
+    refresh = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        exclude = ['password']
+
+    def get_access(self, obj):
+        token = RefreshToken.for_user(obj)
+
+        token['id'] = obj.id
+        token['first_name'] = obj.first_name
+        token['last_name'] = obj.last_name
+        token['email'] = obj.email
+        return str(token.access_token)
+    
+    def get_refresh(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token)
+        
