132237ceffeded0ba2bc3e217ece753b9e354d2b
Motorenger <90602764+Motorenger@users.noreply.github.com>
2023-01-30 21:59:09 +0200

    CV-38-made-basic-movie-saving

--- app/whatchlists/utills.py
+++ app/whatchlists/utills.py
@@ -1,6 +1,9 @@
 import os
+from datetime import datetime
 
 import requests
+
+from whatchlists.models import Movie
 
 
 def get_omdb_by_search(search: str) -> dict:
@@ -11,3 +14,47 @@ def get_omdb_by_search(search: str) -> dict:
 def get_omdb_by_omdbid(omdb_id: str):
     data = requests.get(f"https://www.omdbapi.com?apikey={os.environ.get('API_KEY')}&i={omdb_id}").json()
     return data
+
+
+def save_to_db(data: dict):
+    '''Function takes data returned by imdb,
+       distinguishes wheter it is movie of series,
+       than saves it to the database and return
+       created instance
+    '''
+
+    # subfunction to save movie
+    def save_movie(movie):
+        # extracting only info needed for movie model
+        needed_data = {}
+        needed_data['title'] = movie['Title']
+
+        # converting date format
+        released = datetime.strptime(movie['Released'], '%d %b %Y')
+        needed_data['released'] = released
+
+        # editing runtime field
+        needed_data['runtime'] = movie['Runtime'].split(' ')[0]
+
+        needed_data['omdb_id'] = movie['imdbID']
+
+        # initiation of the movie instance and saving
+        movie = Movie(**needed_data)
+        movie.save()
+        return movie
+
+    # subfunction to save series
+    def save_series(series):
+        pass
+
+    # sorting by type (movie or series)
+    data_type = data.get('Type', None)
+    if data_type:
+        if data_type == 'movie':
+            movie = save_movie(data)
+            return movie
+
+        elif data_type == 'series':
+            pass
+
+    return None
