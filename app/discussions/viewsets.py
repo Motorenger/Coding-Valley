988a2f00dfbc14f84acad682feb41e33c287613a
Nazarii Kuntsevych <nazarii.kuntsevych@gmail.com>
2023-01-31 07:57:01 +0200

    CV-42-add-permissions-to-Discussion-and-Comment-models

--- app/discussions/viewsets.py
+++ app/discussions/viewsets.py
@@ -1,14 +1,18 @@
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
 from discussions.models import Discussion, Comment
 from discussions.serializers import DiscussionSerializer, CommentSerializer
+from discussions.permissions import IsOwnerOrIsAdminOrReadOnly
 
 
 class DiscussionViewSet(ModelViewSet):
     queryset = Discussion.objects.all()
     serializer_class = DiscussionSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrIsAdminOrReadOnly]
 
 
 class CommentViewSet(ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrIsAdminOrReadOnly]
