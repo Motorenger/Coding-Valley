2922affe77eacd95d6ec971eddd687abcc2fad10
Nazarii Kuntsevych <nazarii.kuntsevych@gmail.com>
2023-02-01 05:51:53 +0200

    CV-43-add-test_update_with_owner()-test

--- app/discussions/tests/test_viewsets.py
+++ app/discussions/tests/test_viewsets.py
@@ -99,3 +99,16 @@ class TestDiscussionViewSet:
         # THEN
         assert response.status_code == 403, "Status code of response must be 403"
         assert response.json().get('title') != 'new title', "The response object must not contain 'new title'"
+
+    def test_update_with_owner(self, api_client, owner):
+        # GIVEN
+        new_discussion = {'title': 'new title', 'content': 'new content'}
+        discussion = baker.prepare("discussions.Discussion")
+        discussion.user = owner
+        discussion.save()
+        api_client.force_authenticate(user=owner)
+        # WHEN
+        response = api_client.put(reverse("discussions_app:discussions-detail", args=(discussion.id,)), new_discussion)
+        # THEN
+        assert response.status_code == 200, "Status code of response must be 200"
+        assert response.json().get('title') == 'new title', "The response object must contain 'new title'"
