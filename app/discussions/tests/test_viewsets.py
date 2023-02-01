bc3518dfc78e55cdb166a9297149a5474838721e
Nazarii Kuntsevych <nazarii.kuntsevych@gmail.com>
2023-02-01 05:56:07 +0200

    CV-43-add-test_destroy_with_not_owner()-test

--- app/discussions/tests/test_viewsets.py
+++ app/discussions/tests/test_viewsets.py
@@ -186,3 +186,16 @@ class TestDiscussionViewSet:
         # THEN
         assert delete_response.status_code == 401, "Status code of response must be 401"
         assert len(get_response.json()) == 1, "The response must contain one discussion"
+
+    def test_destroy_with_not_owner(self, api_client, owner, not_owner):
+        # GIVEN
+        discussion = baker.prepare("discussions.Discussion")
+        discussion.user = owner
+        discussion.save()
+        api_client.force_authenticate(user=not_owner)
+        # WHEN
+        delete_response = api_client.delete(reverse("discussions_app:discussions-detail", args=(discussion.id,)))
+        get_response = api_client.get(reverse("discussions_app:discussions-list"))
+        # THEN
+        assert delete_response.status_code == 403, "Status code of response must be 403"
+        assert len(get_response.json()) == 1, "The response must contain one discussion"
