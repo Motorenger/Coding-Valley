2633eeb9ee64ed072579779ed06c966957c9cca5
Nazarii Kuntsevych <nazarii.kuntsevych@gmail.com>
2023-02-01 05:48:50 +0200

    CV-43-add-test_list_with_authenticated_user()-test

--- app/discussions/tests/test_viewsets.py
+++ app/discussions/tests/test_viewsets.py
@@ -22,3 +22,13 @@ class TestDiscussionViewSet:
         # THEN
         assert response.status_code == 200, "Status code of response must be 200"
         assert len(response.json()) == 1, "The response must contain one discussion"
+
+    def test_list_with_authenticated_user(self, api_client, user):
+        # GIVEN
+        baker.make("discussions.Discussion")
+        api_client.force_authenticate(user=user)
+        # WHEN
+        response = api_client.get(reverse("discussions_app:discussions-list"))
+        # THEN
+        assert response.status_code == 200, "Status code of response must be 200"
+        assert len(response.json()) == 1, "The response must contain one discussion"
