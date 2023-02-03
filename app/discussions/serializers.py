0e2a068a2052d35760a526aff19ffa25b7259e2b
Nazarii Kuntsevych <nazarii.kuntsevych@gmail.com>
2023-02-03 06:35:52 +0200

    CV-33-add-comments-pagination-for-DiscussionSerializerWithComments

--- app/discussions/serializers.py
+++ app/discussions/serializers.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from rest_framework import serializers
 
 from discussions.models import Discussion, Comment
@@ -10,9 +11,19 @@ class DiscussionSerializer(serializers.ModelSerializer):
 
 
 class DiscussionSerializerWithComments(serializers.ModelSerializer):
+    comments_new = serializers.SerializerMethodField('paginated_comments')
+
     class Meta:
         model = Discussion
-        fields = ['id', 'title', 'content', 'created', 'updated', 'user', 'movie', 'comments']
+        fields = ['id', 'title', 'content', 'created', 'updated', 'user', 'movie', 'comments', 'comments_new']
+
+    def paginated_comments(self, obj):
+        page_size = self.context['request'].query_params.get('size') or 5
+        page_number = self.context['request'].query_params.get('page') or 1
+        paginator = Paginator(obj.comments.all(), page_size)
+        data = paginator.page(page_number)
+        serializer = CommentSerializer(data, many=True)
+        return serializer.data
 
 
 class CommentSerializer(serializers.ModelSerializer):
