caf4d0feb4e0849d5a688ca4260b85754acaa112
Igor Karuna <88438873+igorkaruna@users.noreply.github.com>
2023-02-05 21:21:33 +0200

    rewrite-permissions

--- app/users/permissions.py
+++ app/users/permissions.py
@@ -1,25 +1,15 @@
 from rest_framework import permissions
 
 
-class IsOwnerOrSuperuser(permissions.BasePermission):
-    @staticmethod
-    def _is_superuser(request):
-        return request.user.is_superuser
-
+class IsOwnerOrIsAdminOrReadOnly(permissions.BasePermission):
+    """
+    Allows the object to be updated or deleted by object's owner or admin.
+    Other users can only read the object.
+    """
     def has_permission(self, request, view):
         return request.user
 
     def has_object_permission(self, request, view, obj):
-        return obj.user == request.user or self._is_superuser(request)
-
-
-class IsOwnerOrIsAdminOrReadOnly(permissions.BasePermission):
-    """Allow to the object be updated or deleted either owner or admin.
-
-    Other users can only read the object.
-    """
-    def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-
-        return request.user == obj.user or request.user.is_superuser
+        return obj.user == request.user or request.user.is_superuser
