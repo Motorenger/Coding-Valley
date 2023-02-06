558bdb297bb6540890669642eedc4e8ac3c46cdb
Nazarii Kuntsevych <nazarii.kuntsevych@gmail.com>
2023-02-06 12:53:12 +0200

    CV-63-add-ReviewLikes-api

--- app/reviews/urls.py
+++ app/reviews/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
-from reviews.viewsets import ReviewViewSet
+from reviews.viewsets import ReviewViewSet, ReviewLikesViewSet
 
 
 router = SimpleRouter()
 router.register(r'reviews', ReviewViewSet, basename='reviews')
+router.register(r'reviewlikes', ReviewLikesViewSet, basename='reviewlikes')
 
 app_name = "reviews_app"
 
