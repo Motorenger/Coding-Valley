1e7f62f338ddb2a610896e6b52bbe11fe599cfad
Igor Karuna <ihor.karuna@gmail.com>
2023-02-11 13:15:15 +0200

    cv-73-add-redis-environ-delete-unused-cache

--- app/discussions/signals.py
+++ app/discussions/signals.py
@@ -4,7 +4,7 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.core.cache import cache
 
-from discussions.models import Discussion, Comment
+from discussions.models import Discussion
 
 
 logger = logging.getLogger(__name__)
@@ -14,13 +14,5 @@ logger = logging.getLogger(__name__)
 def clear_discussions_cache(sender, instance, **kwargs):
     for key in cache.keys('*'):
         if 'discussions_viewset' in key.split('.'):
-            cache.delete(key)
-            logger.info(f'{sender.__name__}’s key: {key} was deleted')
-
-
-@receiver(post_save, sender=Comment)
-def clear_comments_cache(sender, instance, **kwargs):
-    for key in cache.keys('*'):
-        if 'comments_viewset' in key.split('.'):
             cache.delete(key)
             logger.info(f'{sender.__name__}’s key: {key} was deleted')
