e62cb28291d1aff2bc1045582c225498ddba2dc7
Nazarii Kuntsevych <nazarii.kuntsevych@gmail.com>
2023-02-13 14:24:51 +0200

    CV-58-add-tests-for-users-app

---
+++ app/users/tests/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from django.contrib.auth.hashers import make_password
+from rest_framework.test import APIClient
+
+from users.models import User
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.fixture
+def registered_user():
+    user = User.objects.create(
+        first_name="user_first_name",
+        last_name="user_last_name",
+        email="user_email@email.com",
+        password=make_password("user_password")
+    )
+    return user
