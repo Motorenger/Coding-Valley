933f48f8dfa56938f2e2bbca781c07c3f04f0b45
Motorenger <90602764+Motorenger@users.noreply.github.com>
2023-02-14 13:36:01 +0200

    prduction

--- app/base/settings.py
+++ app/base/settings.py
@@ -105,7 +105,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATIC_ROOT = "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- app/reviews/admin.py
+++ app/reviews/admin.py
@@ -7,14 +7,6 @@ class ReviewLikesInline(admin.TabularInline):
     model = Review.likes.through
 
 
-# @admin.register(Comment)
-# class CommentAdmin(admin.ModelAdmin):
-#     list_display = ("user", "discussion", "created")
-#     list_filter = ("discussion",)
-#     search_fields = ("user", "discussion")
-#     fields = ("user", "discussion", "content")
-
-
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ("user", "media", "stars", "created", "get_likes")
